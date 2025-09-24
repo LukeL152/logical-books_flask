a0419163e2c3b33fa2e9aaccb45cb746c280354f
Lucas Laszacs <lucas@Lucass-MacBook-Air.local>
2025-09-24 17:04:30 -0400

    fix(db): Merge divergent migration histories

---
+++ migrations/versions/84f4c929479a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 84f4c929479a
+Revises: 9211f3697ed4, 930b35bb04ed
+Create Date: 2025-09-24 17:04:02.115878
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '84f4c929479a'
+down_revision = ('9211f3697ed4', '930b35bb04ed')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
